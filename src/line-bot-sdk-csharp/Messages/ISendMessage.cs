40142cb5ca62beadf06d95517b51ddbb3949d192
kaminaka rikuya <rikupin1105@gmail.com>
2021-09-01 00:19:57 +0900

    Update ISendMessage.cs

--- src/line-bot-sdk-csharp/Messages/ISendMessage.cs
+++ src/line-bot-sdk-csharp/Messages/ISendMessage.cs
@@ -3,10 +3,6 @@
     public interface ISendMessage
     {
         MessageType Type { get; }
-
-        /// <summary>
-        /// These properties are used for the quick reply feature. Supported on LINE 8.11.0 and later for iOS and Android. For more information, see Using quick replies.
-        /// </summary>
         QuickReply QuickReply { get; set; }
         Sender Sender { get; set; }
     }
