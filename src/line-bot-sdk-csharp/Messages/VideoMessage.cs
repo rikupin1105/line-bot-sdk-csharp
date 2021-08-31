8b3e108a986ef710cabe6ba7152f1d43a4a7d19a
kaminaka rikuya <rikupin1105@gmail.com>
2021-09-01 00:20:16 +0900

    Update VideoMessage.cs

--- src/line-bot-sdk-csharp/Messages/VideoMessage.cs
+++ src/line-bot-sdk-csharp/Messages/VideoMessage.cs
@@ -1,58 +1,49 @@
 ﻿namespace LineMessagingAPI
 {
     /// <summary>
-    /// Video
-    /// https://developers.line.me/en/docs/messaging-api/reference/#video
+    /// https://developers.line.biz/ja/reference/messaging-api/#video-message
+    /// https://developers.line.biz/en/reference/messaging-api/#video-message
     /// </summary>
     public class VideoMessage : ISendMessage
     {
-        public MessageType Type { get; } = MessageType.Video;
-
         /// <summary>
-        /// These properties are used for the quick reply feature
+        /// Video
         /// </summary>
+        public MessageType Type { get; } = MessageType.Video;
         public QuickReply QuickReply { get; set; }
         public Sender Sender { get; set; }
 
         /// <summary>
         /// URL of video file (Max: 2000 characters)
-        /// HTTPS
+        /// HTTPS over TLS 1.2 or later
         /// mp4
-        /// Max: 10 MB
+        /// Max file size: 10 MB
         /// </summary>
         public string OriginalContentUrl { get; }
 
         /// <summary>
         /// URL of preview image (Max: 2000 characters)
-        /// HTTPS
-        /// JPEG PNG
-        /// Max: 1 MB
+        /// HTTPS over TLS 1.2 or later
+        /// JPEG or PNG
+        /// Max file size: 1 MB
         /// </summary>
         public string PreviewImageUrl { get; set; }
 
         /// <summary>
-        /// 動画視聴完了イベントが発生したときに、動画を識別するためのID。trackingIdを付与した動画メッセージを送信した場合に限り、ユーザーが動画の視聴を完了すると動画視聴完了イベントが発生します。
+        /// ID used to identify the video when Video viewing complete event occurs. If you send a video message with trackingId added, the video viewing complete event occurs when the user finishes watching the video.
+        /// Max: 100 characters
+        /// Supported character types: Half-width alphanumeric characters (a-z, A-Z, 0-9) and symbols (-.=,+*()%$&;:@{}!?<>[])
         /// </summary>
         public string TrackingId { get; set; }
 
         /// <summary>
         /// Constructor
         /// </summary>
-        /// <param name="originalContentUrl">
-        /// URL of video file (Max: 2000 characters)
-        /// HTTPS
-        /// mp4
-        /// Max: 10 MB
-        /// </param>
-        /// <param name="previerImageUrl">
-        /// URL of preview image (Max: 2000 characters)
-        /// HTTPS
-        /// JPEG PNG
-        /// Max: 1 MB
-        /// </param>
-        /// <param name="quickReply">
-        /// QuickReply
-        /// </param>
+        /// <param name="originalContentUrl"></param>
+        /// <param name="previerImageUrl"></param>
+        /// <param name="trackingId"></param>
+        /// <param name="quickReply"></param>
+        /// <param name="sender"></param>
         public VideoMessage(string originalContentUrl, string previerImageUrl, string trackingId = null, QuickReply quickReply = null, Sender sender = null)
         {
             OriginalContentUrl = originalContentUrl;
