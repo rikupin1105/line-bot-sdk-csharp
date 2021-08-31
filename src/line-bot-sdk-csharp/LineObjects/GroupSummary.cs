04fe4d853e578e5ed52b78b9597c586b651f1dd6
kaminaka rikuya <rikupin1105@gmail.com>
2021-08-31 23:18:14 +0900

    Update GroupSummary.cs

--- src/line-bot-sdk-csharp/LineObjects/GroupSummary.cs
+++ src/line-bot-sdk-csharp/LineObjects/GroupSummary.cs
@@ -1,14 +1,21 @@
-﻿using System.Collections.Generic;
-
-namespace LineMessagingAPI
+﻿namespace LineMessagingAPI
 {
     /// <summary>
     /// Response from Get group member user IDs API
     /// </summary>
     public class GroupSummary
     {
+        /// <summary>
+        /// Group ID
+        /// </summary>
         public string GroupId { get; set; }
+        /// <summary>
+        /// Group name
+        /// </summary>
         public string GroupName { get; set; }
+        /// <summary>
+        /// Group icon URL
+        /// </summary>
         public string PictureUrl { get; set; }
     }
 }
