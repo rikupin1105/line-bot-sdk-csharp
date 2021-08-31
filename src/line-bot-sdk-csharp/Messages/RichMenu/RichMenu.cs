a12715d868bd00adc1b9bd1d5862d9ee40ce3060
kaminaka rikuya <rikupin1105@gmail.com>
2021-09-01 00:20:09 +0900

    Update RichMenu.cs

--- src/line-bot-sdk-csharp/Messages/RichMenu/RichMenu.cs
+++ src/line-bot-sdk-csharp/Messages/RichMenu/RichMenu.cs
@@ -4,18 +4,15 @@ using System.Collections.Generic;
 namespace LineMessagingAPI
 {
     /// <summary>
-    /// Rich menu object
     /// https://developers.line.biz/ja/reference/messaging-api/#rich-menu-object
+    /// https://developers.line.biz/en/reference/messaging-api/#rich-menu-object
     /// </summary>
     public class RichMenu
     {
-        private string _name;
-        private string _chatBarText;
-
         /// <summary>
         /// size object which contains the width and height of the rich menu displayed in the chat. Rich menu images must be one of the following sizes: 2500x1686, 2500x843.
         /// </summary>
-        public ImagemapSize Size { get; set; }
+        public ActionSize Size { get; set; }
 
         /// <summary>
         /// true to display the rich menu by default. Otherwise, false.
@@ -25,31 +22,39 @@ namespace LineMessagingAPI
         /// <summary>
         /// Name of the rich menu. This value can be used to help manage your rich menus and is not displayed to users. Maximum of 300 characters.
         /// </summary>
-        public string Name
-        {
-            get => _name;
-            set
-            {
-                _name = value?.Substring(0, Math.Min(value.Length, 300));
-            }
-        }
+        public string Name { get; set; }
 
         /// <summary>
         /// Text displayed in the chat bar. Maximum of 14 characters.
         /// </summary>
-        public string ChatBarText
-        {
-            get => _chatBarText;
-            set
-            {
-                _chatBarText = value?.Substring(0, Math.Min(value.Length, 14));
-            }
-        }
+        public string ChatBarText { get; set; }
 
         /// <summary>
         /// Array of area objects which define the coordinates and size of tappable areas. Maximum of 20 area objects.
         /// </summary>
         public IList<ActionArea> Areas { set; get; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="size"></param>
+        /// <param name="selected"></param>
+        /// <param name="name"></param>
+        /// <param name="chatBarText"></param>
+        /// <param name="areas"></param>
+        public RichMenu(ActionSize size, bool selected, string name, string chatBarText, IList<ActionArea> areas)
+        {
+            Size = size;
+            Selected = selected;
+            Name = name.Substring(0, Math.Min(name.Length, 300));
+            ChatBarText = chatBarText.Substring(0, Math.Min(chatBarText.Length, 14));
+            Areas = areas;
+        }
+
+        public RichMenu()
+        {
+        }
+
 
         /// <summary>
         /// Converts from RichMenu to ResponseRichMenu
