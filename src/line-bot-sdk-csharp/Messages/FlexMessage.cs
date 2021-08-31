6f993c0b4ef1293e25f7a0c7f169653ffbd6faac
kaminaka rikuya <rikupin1105@gmail.com>
2021-09-01 00:19:48 +0900

    Update FlexMessage.cs

--- src/line-bot-sdk-csharp/Messages/FlexMessage.cs
+++ src/line-bot-sdk-csharp/Messages/FlexMessage.cs
@@ -2,13 +2,34 @@
 
 namespace LineMessagingAPI
 {
+    /// <summary>
+    /// https://developers.line.biz/ja/reference/messaging-api/#flex-message
+    /// https://developers.line.biz/en/reference/messaging-api/#flex-message
+    /// </summary>
     public class FlexMessage : ISendMessage
     {
+        /// <summary>
+        /// Flex
+        /// </summary>
         public MessageType Type { get; } = MessageType.Flex;
         public QuickReply QuickReply { get; set; }
-        public IFlexContainer Contents { get; set; }
         public Sender Sender { get; set; }
+        /// <summary>
+        /// Alternative text (Max: 400 characters)
+        /// </summary>
         public string AltText { get; set; }
+        /// <summary>
+        /// Flex Message container
+        /// </summary>
+        public IFlexContainer Contents { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="altText"></param>
+        /// <param name="contents"></param>
+        /// <param name="quickReply"></param>
+        /// <param name="sender"></param>
         public FlexMessage(string altText, IFlexContainer contents, QuickReply quickReply = null, Sender sender = null)
         {
             QuickReply = quickReply;
