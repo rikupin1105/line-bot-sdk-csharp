de914d213b5ab545776facc36a8146519391347a
kaminaka rikuya <rikupin1105@gmail.com>
2021-09-01 00:19:42 +0900

    Create BaseSize.cs

---
+++ src/line-bot-sdk-csharp/LineObjects/BaseSize.cs
@@ -0,0 +1,22 @@
+﻿namespace LineMessagingAPI
+{
+    public class BaseSize
+    {
+        /// <summary>
+        /// Width
+        /// </summary>
+        public int Width { get; }
+
+        /// <summary>
+        /// Height
+        /// </summary>
+        public int Height { get; }
+
+        public BaseSize(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+}
+    
