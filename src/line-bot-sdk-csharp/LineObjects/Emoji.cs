01f1a9c8ce11e1cea076610205624d272531d990
kaminaka rikuya <rikupin1105@gmail.com>
2021-08-31 23:16:50 +0900

    Update Emoji.cs

--- src/line-bot-sdk-csharp/LineObjects/Emoji.cs
+++ src/line-bot-sdk-csharp/LineObjects/Emoji.cs
@@ -2,16 +2,24 @@
 {
     public class Emoji
     {
-        public Emoji(int index,int length,string productId,string emojiId)
+        public Emoji(int index, string productId, string emojiId)
         {
             Index = index;
-            Length = length;
             ProductId = productId;
             EmojiId = emojiId;
         }
+        /// <summary>
+        /// The index position for $ indicating the placeholder for LINE emojis in text, with the first character being at position 0. See the text message example for details.
+        /// Note: If you specify a position that doesn't match the position of $, the API returns HTTP 400 Bad request.
+        /// </summary>
         public int Index { get; set; }
-        public int Length { get; set; }
+        /// <summary>
+        /// Product ID for a set of LINE emoji. For more information on product IDs, see List of available LINE emojis.
+        /// </summary>
         public string ProductId { get; set; }
+        /// <summary>
+        /// Emoji ID. For more information on emoji IDs for LINE emojis that are sendable with the Messaging API, see List of available LINE emojis.
+        /// </summary>
         public string EmojiId { get; set; }
     }
 }
