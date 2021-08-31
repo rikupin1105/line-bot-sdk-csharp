4b5067f1e207f44e2a6ee889919063895afc7d6c
kaminaka rikuya <rikupin1105@gmail.com>
2021-09-01 00:20:06 +0900

    Create ActionSize.cs

---
+++ src/line-bot-sdk-csharp/Messages/RichMenu/ActionSize.cs
@@ -0,0 +1,28 @@
+﻿namespace LineMessagingAPI
+{
+    /// <summary>
+    /// https://developers.line.biz/ja/reference/messaging-api/#size-object
+    /// https://developers.line.biz/en/reference/messaging-api/#size-object
+    /// </summary>
+    public class ActionSize
+    {
+        /// <summary>
+        /// Width of the rich menu. Must be 2500.
+        /// </summary>
+        public int Width { get; set; }
+        /// <summary>
+        /// Height of the rich menu. Possible values: 1686, 843.
+        /// </summary>
+        public int Height { get; set; }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        public ActionSize(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+}
