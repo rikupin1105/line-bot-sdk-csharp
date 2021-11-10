b39b90496936dbc16843514df68975457c80fc00
kaminaka rikuya <rikupin1105@gmail.com>
2021-11-10 19:40:19 +0900

    友だちのユーザーIDを一度に最大1000件取得できるようになりました

--- src/line-bot-sdk-csharp/ILineMessagingClient.cs
+++ src/line-bot-sdk-csharp/ILineMessagingClient.cs
@@ -168,6 +168,7 @@ namespace LineMessagingAPI
         /// <param name="userId">User ID</param>
         /// <returns></returns>
         Task<UserProfile> GetUserProfileAsync(string userId);
+        Task<FollowerIds> GetFollowerIdsAsync(int limit, string start);
 
         #endregion
 
@@ -209,7 +210,7 @@ namespace LineMessagingAPI
         /// </summary>
         /// <param name="groupId">Identifier of the group</param>
         /// <param name="continuationToken">ContinuationToken</param>
-        Task<GroupMemberIds> GetGroupMemberIdsAsync(string groupId, string continuationToken=null);
+        Task<GroupMemberIds> GetGroupMemberIdsAsync(string groupId, string continuationToken = null);
 
         /// <summary>
         /// グループメンバーのプロフィール情報を取得する
---
+++ src/line-bot-sdk-csharp/LineObjects/FollowerIds.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace LineMessagingAPI
+{
+    /// <summary>
+    /// Response from Get Followers IDs API
+    /// </summary>
+    public class FollowerIds
+    {
+        /// <summary>
+        /// List of user IDs of the followers.
+        /// Max: 1000 user IDs
+        /// The default value is 300
+        /// </summary>
+        public IList<string> UserIds { get; set; }
+
+        /// <summary>
+        /// continuationToken
+        /// A continuation token to get the next array of user IDs.
+        /// </summary>
+        public string Next { get; set; }
+    }
+}
