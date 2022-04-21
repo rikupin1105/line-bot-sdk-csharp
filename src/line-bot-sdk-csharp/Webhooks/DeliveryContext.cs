96b5aaf280d9815c89623a9c365374b785060d72
kaminaka rikuya <rikupin1105@gmail.com>
2022-04-22 00:06:35 +0900

    Create DeliveryContext.cs

---
+++ src/line-bot-sdk-csharp/Webhooks/DeliveryContext.cs
@@ -0,0 +1,11 @@
+﻿namespace LineMessagingAPI
+{
+    public class DeliveryContext
+    {
+        public DeliveryContext(bool isRedelivery)
+        {
+            IsRedelivery = isRedelivery;
+        }
+        public bool IsRedelivery { get; }
+    }
+}
