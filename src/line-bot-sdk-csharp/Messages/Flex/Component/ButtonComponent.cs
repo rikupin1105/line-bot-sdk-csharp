3337936163110de3bc6d34ed02990e689f6ddb82
kaminaka rikuya <rikupin1105@gmail.com>
2023-07-14 22:04:05 +0900

    任意の項目を null 許容型へ、一部コンストラクタを削除

--- src/line-bot-sdk-csharp/Messages/Flex/Component/ButtonComponent.cs
+++ src/line-bot-sdk-csharp/Messages/Flex/Component/ButtonComponent.cs
@@ -5,25 +5,21 @@
         public FlexComponentType Type => FlexComponentType.Button;
         public ITemplateAction Action { get; set; }
         public int? Flex { get; set; }
-        public string Margin { get; set; }
+        public string? Margin { get; set; }
         public Position? Position { get; set; }
-        public string OffsetTop { get; set; }
-        public string OffsetBottom { get; set; }
-        public string OffsetStart { get; set; }
-        public string OffsetEnd { get; set; }
-        public string Height { get; set; }
+        public string? OffsetTop { get; set; }
+        public string? OffsetBottom { get; set; }
+        public string? OffsetStart { get; set; }
+        public string? OffsetEnd { get; set; }
+        public string? Height { get; set; }
         public ButtonStyle? Style { get; set; }
-        public string Color { get; set; }
+        public string? Color { get; set; }
         public Gravity? Gravity { get; set; }
-        public string AdjustMode { get; set; }
+        public string? AdjustMode { get; set; }
         public bool? Scaling { get; set; }
         public ButtonComponent(ITemplateAction action)
         {
             Action = action;
-        }
-        public ButtonComponent()
-        {
-
         }
     }
 }
