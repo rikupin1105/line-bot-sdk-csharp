691fba4d2da1a6c31f8d674a3b0c0ace20474f6c
kaminaka rikuya <rikupin1105@gmail.com>
2023-07-14 22:02:11 +0900

    任意の項目を null 許容型へ

--- src/line-bot-sdk-csharp/Messages/Flex/Component/TextComponent.cs
+++ src/line-bot-sdk-csharp/Messages/Flex/Component/TextComponent.cs
@@ -5,28 +5,28 @@ namespace LineMessagingAPI
     public class TextComponent : IFlexComponent
     {
         public FlexComponentType Type => FlexComponentType.Text;
-        public string Text { get; set; }
-        public IList<SpanComponent> Contents { get; set; }
-        public string AdjustMode { get; set; }
+        public string? Text { get; set; }
+        public IList<SpanComponent>? Contents { get; set; }
+        public string? AdjustMode { get; set; }
         public int? Flex { get; set; }
-        public string Margin { get; set; }
+        public string? Margin { get; set; }
         public Position? Position { get; set; }
-        public string OffsetTop { get; set; }
-        public string OffsetBottom { get; set; }
-        public string OffsetStart { get; set; }
-        public string OffsetEnd { get; set; }
-        public string Size { get; set; }
+        public string? OffsetTop { get; set; }
+        public string? OffsetBottom { get; set; }
+        public string? OffsetStart { get; set; }
+        public string? OffsetEnd { get; set; }
+        public string? Size { get; set; }
         public bool? Scalling { get; set; }
         public Align? Align { get; set; }
         public Gravity? Gravity { get; set; }
         public bool? Wrap { get; set; }
-        public string LineSpacing { get; set; }
+        public string? LineSpacing { get; set; }
         public int? MaxLines { get; set; }
         public Weight? Weight { get; set; }
-        public string Color { get; set; }
-        public ITemplateAction Action { get; set; }
+        public string? Color { get; set; }
+        public ITemplateAction? Action { get; set; }
         public TextStyle? Style { get; set; }
-        public string Decoration { get; set; }
+        public string? Decoration { get; set; }
         public TextComponent(string text)
         {
             Text = text;
