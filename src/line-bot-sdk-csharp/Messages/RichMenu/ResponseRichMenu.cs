51cf7d33f1ba0aaeac6cf57b1bfceb68b3d8dda1
kaminaka rikuya <rikupin1105@gmail.com>
2023-07-14 22:15:26 +0900

    null対応

--- src/line-bot-sdk-csharp/Messages/RichMenu/ResponseRichMenu.cs
+++ src/line-bot-sdk-csharp/Messages/RichMenu/ResponseRichMenu.cs
@@ -31,22 +31,26 @@ namespace LineMessagingAPI
 
         internal static ResponseRichMenu CreateFrom(dynamic dynamicObject)
         {
+            if(dynamicObject == null)
+            {
+                throw new System.NullReferenceException();
+            }
 
             var areas = new List<ActionArea>();
-            foreach (var area in dynamicObject?.areas ?? Enumerable.Empty<dynamic>())
+            foreach (var area in dynamicObject.areas ?? Enumerable.Empty<dynamic>())
             {
                 areas.Add(ActionArea.CreateFrom(area));
             }
 
             var menu = new RichMenu()
             {
-                Name = (string)dynamicObject?.name,
-                Size = new ActionSize((int)(dynamicObject?.size?.width ?? 0), (int)(dynamicObject?.size?.height ?? 0)),
-                Selected = (bool)(dynamicObject?.selected ?? false),
-                ChatBarText = (string)dynamicObject?.chatBarText,
+                Name = (string)dynamicObject.name,
+                Size = new ActionSize((int)(dynamicObject.size?.width ?? 0), (int)(dynamicObject.size?.height ?? 0)),
+                Selected = (bool)(dynamicObject.selected ?? false),
+                ChatBarText = (string)dynamicObject.chatBarText,
                 Areas = areas
             };
-            return new ResponseRichMenu((string)dynamicObject?.richMenuId, menu);
+            return new ResponseRichMenu((string)dynamicObject.richMenuId, menu);
         }
     }
 }
